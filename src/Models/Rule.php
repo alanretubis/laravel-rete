0533cce2da41b1b30c6b47791fffeadd818661b2
Alan Teves Retubis <alanretubis@gmail.com>
2025-04-23 00:24:24 +0800

    :hammer: Updated Rule Model.

--- src/Models/Rule.php
+++ src/Models/Rule.php
@@ -6,4 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 class Rule extends Model
 {
     protected $fillable = ['name', 'conditions', 'actions'];
+
+    protected $casts = [
+        'conditions' => 'array', // Automatically cast conditions to an array
+        'actions' => 'array',    // Automatically cast actions to an array
+    ];
 }
